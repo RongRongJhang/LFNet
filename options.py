d759748f77a712fb8f8579cec210f4840fbc7e88
RongRongJhang <dandelionron@gmail.com>
2025-05-17 18:32:59 +0800

    "modify data folder"

--- options.py
+++ options.py
@@ -11,31 +11,31 @@ def option():
     parser.add_argument('--threads', type=int, default=4, help='number of threads for dataloader to use')
 
     # train input
-    parser.add_argument('--data_train_lol_v1'       , type=str, default='data/LOLdataset/our485/low')
-    parser.add_argument('--data_train_lolv2_real'   , type=str, default='data/LOLv2/Real_captured/Train/Low')
-    parser.add_argument('--data_train_lolv2_syn'    , type=str, default='data/LOLv2/Synthetic/Train/Low')
+    parser.add_argument('--data_train_lol_v1'       , type=str, default='datasets/LOLdataset/our485/low')
+    parser.add_argument('--data_train_lolv2_real'   , type=str, default='dadatasetsta/LOLv2/Real_captured/Train/Low')
+    parser.add_argument('--data_train_lolv2_syn'    , type=str, default='datasets/LOLv2/Synthetic/Train/Low')
 
     # train ground truth
-    parser.add_argument('--data_traingt_lol_v1'       , type=str, default='data/LOLdataset/our485/high')
-    parser.add_argument('--data_traingt_lolv2_real'   , type=str, default='data/LOLv2/Real_captured/Train/Normal')
-    parser.add_argument('--data_traingt_lolv2_syn'    , type=str, default='data/LOLv2/Synthetic/Train/Normal')
+    parser.add_argument('--data_traingt_lol_v1'       , type=str, default='datasets/LOLdataset/our485/high')
+    parser.add_argument('--data_traingt_lolv2_real'   , type=str, default='datasets/LOLv2/Real_captured/Train/Normal')
+    parser.add_argument('--data_traingt_lolv2_syn'    , type=str, default='datasets/LOLv2/Synthetic/Train/Normal')
 
     # validation input
-    parser.add_argument('--data_val_lol_v1'         , type=str, default='data/LOLdataset/eval15/low')
-    parser.add_argument('--data_val_lolv2_real'     , type=str, default='data/LOLv2/Real_captured/Test/Low')
-    parser.add_argument('--data_val_lolv2_syn'      , type=str, default='data/LOLv2/Synthetic/Test/Low')
+    parser.add_argument('--data_val_lol_v1'         , type=str, default='datasets/LOLdataset/eval15/low')
+    parser.add_argument('--data_val_lolv2_real'     , type=str, default='datasets/LOLv2/Real_captured/Test/Low')
+    parser.add_argument('--data_val_lolv2_syn'      , type=str, default='datasets/LOLv2/Synthetic/Test/Low')
 
     # validation ground truth
-    parser.add_argument('--data_valgt_lol_v1'       , type=str, default='data/LOLdataset/eval15/high')
-    parser.add_argument('--data_valgt_lolv2_real'   , type=str, default='data/LOLv2/Real_captured/Test/Normal')
-    parser.add_argument('--data_valgt_lolv2_syn'    , type=str, default='data/LOLv2/Synthetic/Test/Normal')
+    parser.add_argument('--data_valgt_lol_v1'       , type=str, default='datasets/LOLdataset/eval15/high')
+    parser.add_argument('--data_valgt_lolv2_real'   , type=str, default='datasets/LOLv2/Real_captured/Test/Normal')
+    parser.add_argument('--data_valgt_lolv2_syn'    , type=str, default='datasets/LOLv2/Synthetic/Test/Normal')
 
     # unpaired datasets for test
-    parser.add_argument('--data_DICM'  , type=str, default='data/DICM')
-    parser.add_argument('--data_LIME'  , type=str, default='data/LIME')
-    parser.add_argument('--data_MEF'   , type=str, default='data/MEF')
-    parser.add_argument('--data_NPE'   , type=str, default='data/NPE')
-    parser.add_argument('--data_VV'    , type=str, default='data/VV')
+    parser.add_argument('--data_DICM'  , type=str, default='datasets/DICM')
+    parser.add_argument('--data_LIME'  , type=str, default='datasets/LIME')
+    parser.add_argument('--data_MEF'   , type=str, default='datasets/MEF')
+    parser.add_argument('--data_NPE'   , type=str, default='datasets/NPE')
+    parser.add_argument('--data_VV'    , type=str, default='datasets/VV')
 
     # loss weights
     parser.add_argument('--smooth_weight', type=float, default=1.0)
